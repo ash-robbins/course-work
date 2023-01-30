ef9f4eeadcb9fb0daae6a0904d79e7f1fb3137bb
ashley robbins <ashley.robbins22@aol.com>
2023-01-30 16:23:14 -0500

    Exersise 7/9

---
+++ module-1/11_Inheritance/exercise/src/main/java/com/techelevator/CheckingAccount.java
@@ -0,0 +1,22 @@
+package com.techelevator;
+
+import javax.print.DocFlavor;
+
+public class CheckingAccount extends BankAccount {
+
+    public CheckingAccount(String accountHolderName, String accountNumber){
+        super(accountHolderName, accountNumber);
+    }
+
+    public CheckingAccount(String accountHolderName, String accountNumber, int balance){
+        super(accountHolderName, accountNumber, balance);
+
+}
+@Override
+    public int withdraw(int amountToWithdraw) {
+    return super.withdraw(amountToWithdraw);
+}
+
+
+
+}
--- module-1/11_Inheritance/lecture/src/main/java/com/techelevator/Application.java
+++ module-1/11_Inheritance/lecture/src/main/java/com/techelevator/Application.java
@@ -5,17 +5,23 @@ public class Application {
     public static void main(String[] args) {
 
         // Create a new general auction
-        System.out.println("Starting a general auction");
-        System.out.println("-----------------");
+        //System.out.println("Starting a general auction");
+        //System.out.println("-----------------");
 
-        Auction generalAuction = new Auction("Tech Elevator t-shirt");
+       // Auction generalAuction = new Auction("Tech Elevator t-shirt");
 
-        generalAuction.placeBid(new Bid("Josh", 1));
-        generalAuction.placeBid(new Bid("Fonz", 23));
-        generalAuction.placeBid(new Bid("Rick Astley", 13));
+        //generalAuction.placeBid(new Bid("Josh", 1));
+        //generalAuction.placeBid(new Bid("Fonz", 23));
+        //generalAuction.placeBid(new Bid("Rick Astley", 13));
         //....
         //....
         // This might go on until the auction runs out of time or hits a max # of bids
 
+        System.out.println("Starting a buyout auction");
+        System.out.println("-----------------");
+
+        BuyoutAuction buyoutAuction = new BuyoutAuction("Tech Elevator Mug", 20);
+        buyoutAuction.placeBid(new Bid("Tony Stark", 2));
+        buyoutAuction.placeBid(new Bid("Lebron James", 23));
     }
 }
